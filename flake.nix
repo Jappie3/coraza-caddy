bdd62533ac4accd6c50d8dba586b7360edddc2e7
Jappie3 <jasper22034@gmail.com>
2023-12-16 11:27:30 +0100

    feat: use flake-parts

--- flake.nix
+++ flake.nix
@@ -1,22 +1,33 @@
 {
-  description = "A very basic flake";
+  description = "Coraza WAF Caddy Module w/ GeoIP plugin";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
 
   outputs = {
     self,
     nixpkgs,
-  }: let
-    pkgs = import nixpkgs {
-      system = "x86_64-linux";
-      config = {allowUnfree = true;};
-    };
-  in {
-    devShell.${pkgs.system} = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        go
-        gopls
-        gotools
-        go-tools
+    flake-parts,
+    ...
+  } @ inputs:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = [
+        "x86_64-linux"
       ];
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            gopls
+            gotools
+            go-tools
+          ];
+        };
+      };
     };
-  };
 }
